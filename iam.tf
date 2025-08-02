5080fe6b21813dc899b8ff661491928d53430ce0
Roman Vaivod <roman.vaivod@similarweb.com>
2025-08-02 15:41:30 +0200

    chore: terraform init

---
+++ iam.tf
@@ -0,0 +1,10 @@
+resource "aws_iam_user" "terraform_admin" {
+  force_destroy = false
+  name = "terraform-admin"
+  tags = {
+    "device" = "mac-sw-terraform"
+  }
+  tags_all = {
+    "device" = "mac-sw-terraform"
+  }
+}
