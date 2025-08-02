0664e549e954c7181b7d4bf70d971ca77f6c4774
littlewhywhat <littlewhywhat@gmail.com>
2025-08-02 23:05:56 +0200

    cursor: add missing IAM access key resource for GitHub integration

--- modules/agent-hub/outputs.tf
+++ modules/agent-hub/outputs.tf
@@ -7,3 +7,12 @@ output "agent_hub_instance_id" {
   description = "Instance ID of the agent-hub EC2 instance"
   value       = aws_instance.agent_hub.id
 }
+
+output "github_access_key_id" {
+  value = aws_iam_access_key.github_agent_hub_key.id
+}
+
+output "github_secret_access_key" {
+  value     = aws_iam_access_key.github_agent_hub_key.secret
+  sensitive = true
+}
