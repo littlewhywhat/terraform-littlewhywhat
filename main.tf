4c35d10e36afe0c4ef428e94e09d6f2f58ca75f2
littlewhywhat <littlewhywhat@gmail.com>
2025-08-03 15:15:31 +0200

    remove outputs

--- main.tf
+++ main.tf
@@ -4,12 +4,12 @@ module "ec2_service" {
 
 module "agent_hub" {
   source = "./modules/agent-hub"
-  codedeploy_service_role_arn        = module.code-deploy.codedeploy_role_arn
-  agent_hub_ssh_public_key           = var.agent_hub_ssh_public_key
-  amazon_linux_ami_id                = module.ec2_service.amazon_linux_ami_id
-  ec2_service_security_group_id      = module.ec2_service.ec2_service_security_group_id
-  agent_hub_webhook_github_token     = var.agent_hub_webhook_github_token
-  github_management_token            = var.github_management_token
+  codedeploy_service_role_arn            = module.code-deploy.codedeploy_role_arn
+  agent_hub_ssh_public_key               = var.agent_hub_ssh_public_key
+  amazon_linux_ami_id                    = module.ec2_service.amazon_linux_ami_id
+  ec2_service_security_group_id          = module.ec2_service.ec2_service_security_group_id
+  agent_hub_webhook_github_token         = var.agent_hub_webhook_github_token
+  github_management_token                = var.github_management_token
 }
 
 module "code-deploy" {
@@ -19,17 +19,6 @@ module "code-deploy" {
 output "agent_hub_ssh_command" {
   description = "SSH command to connect to agent-hub instance"
   value       = "ssh -i /path/to/your/private/key ec2-user@${module.agent_hub.agent_hub_public_ip}"
-}
-
-output "github_access_key_id" {
-  description = "GitHub IAM access key ID"
-  value       = module.agent_hub.github_access_key_id
-}
-
-output "github_secret_access_key" {
-  description = "GitHub IAM secret access key"
-  value       = module.agent_hub.github_secret_access_key
-  sensitive   = true
 }
 
 output "webhook_github_token_secret_name" {
