ee4866a8a453fb5e67eaf865e162ccad239d703c
littlewhywhat <littlewhywhat@gmail.com>
2025-09-06 14:39:43 +0200

    deleted: destroy python agenthub with secrets

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ module "ec2_service" {
   source = "./modules/ec2-service"
 }
 
+module "code-deploy" {
+  source = "./modules/code-deploy"
+}
+
 module "agent_hub" {
   source = "./modules/agent-hub"
   codedeploy_service_role_arn            = module.code-deploy.codedeploy_role_arn
@@ -13,19 +17,15 @@ module "agent_hub" {
   github_management_token                = var.github_management_token
 }
 
-module "code-deploy" {
-  source = "./modules/code-deploy"
-}
+# output "agent_hub_ssh_command" {
+#   description = "SSH command to connect to agent-hub instance"
+#   value       = "ssh -i ~/.ssh/agent-hub-key ec2-user@${module.agent_hub.agent_hub_public_ip}"
+# }
 
-output "agent_hub_ssh_command" {
-  description = "SSH command to connect to agent-hub instance"
-  value       = "ssh -i ~/.ssh/agent-hub-key ec2-user@${module.agent_hub.agent_hub_public_ip}"
-}
-
-output "webhook_github_token_secret_name" {
-  description = "Name of the agent-hub webhook GitHub token secret - use this to set the secret value"
-  value       = module.agent_hub.webhook_github_token_secret_name
-}
+# output "webhook_github_token_secret_name" {
+#   description = "Name of the agent-hub webhook GitHub token secret - use this to set the secret value"
+#   value       = module.agent_hub.webhook_github_token_secret_name
+# }
 
 output "agent_hub_webhook_github_token" {
   description = "Agent hub webhook GitHub token"
