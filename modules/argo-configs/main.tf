5645417c5226367ea30e5f1a06ab4f53a302b5fd
littlewhywhat <littlewhywhat@gmail.com>
2025-09-16 12:55:13 +0200

    chore: sync github config

--- modules/argo-configs/main.tf
+++ modules/argo-configs/main.tf
@@ -1,6 +1,6 @@
 resource "github_repository" "argo_configs" {
   name         = "argo-configs"
-  visibility   = "public"
+  visibility   = "private"
   
   has_issues   = true
   has_wiki     = false
@@ -10,24 +10,4 @@ resource "github_repository" "argo_configs" {
   allow_squash_merge     = true
   allow_rebase_merge     = true
   delete_branch_on_merge = true
-}
-
-resource "github_actions_repository_permissions" "argo_configs" {
-  repository = github_repository.argo_configs.name
-  
-  enabled = true
-  allowed_actions = "all"
-  
-  allowed_actions_config {
-    github_owned_allowed = true
-    verified_allowed     = true
-    patterns_allowed     = ["*"]
-  }
-}
-
-resource "github_branch_protection" "argo_configs_main" {
-  repository_id = github_repository.argo_configs.name
-  pattern       = "main"
-  
-  enforce_admins = false
 }
