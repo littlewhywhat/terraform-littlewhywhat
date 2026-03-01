ea6ee4b7450df148cd7547baef7230856d2d05fc
littlewhywhat <littlewhywhat@gmail.com>
2026-03-01 23:22:45 +0100

    chore: destroy ec2 instance as unused

--- demo-k3s-cluster.tf
+++ demo-k3s-cluster.tf
@@ -7,39 +7,43 @@ output "argo_configs_github_repository_name" {
   value       = module.argo_configs.github_repository_name
 }
 
-module "demo_k3s_cluster" {
-  source = "./modules/demo-k3s-cluster"
+# module "demo_k3s_cluster" {
+#   source = "./modules/demo-k3s-cluster"
   
-  demo_k3s_cluster_ssh_public_key = var.demo_k3s_cluster_ssh_public_key
-  ubuntu_ami_id                   = "ami-0bbdd8c17ed981ef9"
+#   demo_k3s_cluster_ssh_public_key = var.demo_k3s_cluster_ssh_public_key
+#   ubuntu_ami_id                   = "ami-0bbdd8c17ed981ef9"
   
-  demo_coding_agent_gitlab_token          = var.demo_coding_agent_gitlab_token
-  demo_coding_agent_gitlab_webhook_secret = var.demo_coding_agent_gitlab_webhook_secret
-  demo_coding_agent_cursor_token          = var.demo_coding_agent_cursor_token
-  demo_coding_agent_gitlab_project_id     = var.demo_coding_agent_gitlab_project_id
-  demo_coding_agent_gitlab_username       = var.demo_coding_agent_gitlab_username
-  demo_coding_agent_gitlab_email          = var.demo_coding_agent_gitlab_email
-  demo_coding_agent_slack_app_token       = var.demo_coding_agent_slack_app_token
-  demo_coding_agent_slack_bot_token       = var.demo_coding_agent_slack_bot_token
-}
+#   demo_coding_agent_gitlab_token          = var.demo_coding_agent_gitlab_token
+#   demo_coding_agent_gitlab_webhook_secret = var.demo_coding_agent_gitlab_webhook_secret
+#   demo_coding_agent_cursor_token          = var.demo_coding_agent_cursor_token
+#   demo_coding_agent_gitlab_project_id     = var.demo_coding_agent_gitlab_project_id
+#   demo_coding_agent_gitlab_username       = var.demo_coding_agent_gitlab_username
+#   demo_coding_agent_gitlab_email          = var.demo_coding_agent_gitlab_email
+#   demo_coding_agent_slack_app_token       = var.demo_coding_agent_slack_app_token
+#   demo_coding_agent_slack_bot_token       = var.demo_coding_agent_slack_bot_token
+# }
 
 output "demo_k3s_cluster_ssh_command" {
   description = "SSH command to connect to demo k3s cluster"
-  value       = "ssh -i ~/.ssh/demo-k3s-cluster-key ubuntu@${module.demo_k3s_cluster.public_ip}"
+  # value       = "ssh -i ~/.ssh/demo-k3s-cluster-key ubuntu@${module.demo_k3s_cluster.public_ip}"
+  value = "disabled"
 }
 
 output "demo_k3s_cluster_public_ip" {
   description = "Public IP of demo k3s cluster"
-  value       = module.demo_k3s_cluster.public_ip
+  # value       = module.demo_k3s_cluster.public_ip
+  value = "disabled"
 }
 
 output "argo_workflows_ui" {
   description = "URL to access Argo Workflows UI in browser"
-  value       = module.demo_k3s_cluster.argo_ui_url
+  # value       = module.demo_k3s_cluster.argo_ui_url
+  value = "disabled"
 }
 
 output "argo_admin_token" {
   description = "Admin token for Argo Workflows UI - copy this to login"
-  value       = module.demo_k3s_cluster.argo_admin_token
+  # value       = module.demo_k3s_cluster.argo_admin_token
+  value = "disabled"
   sensitive   = true
 }
